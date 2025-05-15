7fe93c95cb4c00ee5236766e58da9827c904ed59
Yassine Housseine <151876208+Yaskoi@users.noreply.github.com>
2025-05-15 20:58:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='ysk_quantlib',
-    version='0.1.0',
-    author_email='yassine.housseine2@gmail.com',
-    description='Custom quantitative finance library for hedging and derivatives pricing',
+    version='0.1',
+    description='Quant finance library: pricing, greeks, hedging, stats',
     author='Yassine Housseine',
+    author_email='yassine.housseine2@gmail.com',
+    url='https://github.com/yassine-housseine/ysk_quantlib',
     packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
         'scipy',
-        'matplotlib'
+        'matplotlib',
+        'statsmodels',
+        'arch'
     ],
-    include_package_data=True,
-    zip_safe=False
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ]
 )
 
 # It's not really a package, it's just a collection of scripts that I use for my own purposes. 
